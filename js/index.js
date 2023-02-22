94abe5b04235361ca0705cb73122c29f797391eb
Kenzhie123 <Kenzcabute@gmail.com>
2023-02-23 02:31:02 +0800

    change

--- js/index.js
+++ js/index.js
@@ -2,8 +2,18 @@ var menu_button = document.getElementById('menu_button');
 var sidebar = document.getElementById('sidebar');
 
 
+
+
+document.addEventListener('click', (event)=>{
+    if(!sidebar.contains(event.target) && !menu_button.contains(event.target))
+    {
+        sidebar.style.display = 'none';
+        console.log('body clicked');
+    }
+    
+});
+
 menu_button.addEventListener('click', ()=>{
     sidebar.style.display = 'block';
     console.log('menu button clicked');
 });
-
